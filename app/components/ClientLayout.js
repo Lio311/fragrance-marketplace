eccbbc9ddc5300d0f4afbc1e80b7254b74dfbf9c
Lior <lior31197@gmail.com>
2026-07-24 17:29:13 +0300

    Remove pt-16 on homepage to allow hero to reach top

--- app/components/ClientLayout.js
+++ app/components/ClientLayout.js
@@ -1,13 +1,19 @@
 'use client';
 
+import { usePathname } from 'next/navigation';
 import Header from './Header';
 import Footer from './Footer';
 
 export default function ClientLayout({ children }) {
+  const pathname = usePathname();
+  const isHomePage = pathname === '/';
+
   return (
     <>
       <Header />
-      <main className="min-h-screen pt-16">{children}</main>
+      <main className={`min-h-screen ${isHomePage ? '' : 'pt-20'}`}>
+        {children}
+      </main>
       <Footer />
     </>
   );
